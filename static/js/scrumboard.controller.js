edafef6508a95743487cf2f090c6265a9d686367
BillyDoo <billygoat991@gmail.com>
2019-04-18 21:55:12 +0200

    Add files via upload

---
+++ static/js/scrumboard.controller.js
@@ -0,0 +1,35 @@
+(function () {
+    'use strict';
+
+    angular.module('scrumboard.demo')
+        .controller('ScrumboardController',
+                    ['$scope', '$http', '$location', '$routeParams', 'Login', ScrumboardController]);
+
+    function ScrumboardController($scope, $http, $location, $routeParams, Login) {
+        $scope.add = function (list, title) {
+            var card = {
+                list: list.id,
+                owner: Login.currentUser().id,
+                title: title
+            };
+
+            $http.post('/scrumboard/cards/', card)
+                .then(function (response) {
+                    list.cards.push(response.data);
+                },
+                function(){
+                    alert('Could not create card');
+                }
+            );
+
+        };
+
+
+
+
+
+
+
+    }
+
+}());
